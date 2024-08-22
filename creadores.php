0734455c64100d720ab9782772b28950c2f430ca
MARIA GIANA <mariajosegiana@gmail.com>
2024-08-22 11:42:45 -0300

    primer commit

---
+++ creadores.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="styleSheet" href="calculadora.css">
+    <title>Calculadora</title>
+</head>
+<body>
+
+    <nav id="mainNav">
+        <ul id="mainUl">
+            <li> <a href="index.html">CALCULADORA</a></li>
+            <li> <a href="numeroPI.php">NUMERO PI</a></li>
+            <li><a href="creadores.php">About</a></li>
+        </ul>
+    </nav>
+    <h1>ACERCA DE...</h1>
+
+<p>El autor .</p>
+</body>
+</html>
